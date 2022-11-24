d6d37f7b68ae6ee47fb87f6a82c69d56f69afaa5
muhyidin3222 <muhyidin480@gmail.com>
2022-11-24 13:32:32 +0700

    fix api

--- src/callback/callback.service.ts
+++ src/callback/callback.service.ts
@@ -20,50 +20,54 @@ export class CallbackService {
       headers: JSON.stringify(headers),
       body: JSON.stringify(body),
     });
-    const resCreated = body.map((value) => {
-      const {
-        imei,
-        vno,
-        vin,
-        lat,
-        lng,
-        lad,
-        led,
-        bp,
-        sts,
-        spd,
-        odo,
-        pow,
-        ss,
-        ns,
-        phn,
-        eml,
-        alerts,
-        aid,
-        id,
-      }: any = value;
-      return {
-        vehicle_number: vno,
-        imei: imei,
-        vin: vin,
-        lat: lat,
-        lng: lng,
-        last_activation_date: lad,
-        license_expire_date: led,
-        battery_level_percentage: bp,
-        status: sts,
-        speed: spd,
-        odometer: odo,
-        power: pow,
-        service_status: ss,
-        phone_number: phn,
-        email: eml,
-        alerts: JSON.stringify(alerts),
-        asset_id: aid,
-        next_service: ns,
-        id,
-      };
-    });
-    return this.aerisDataRepository.bulkCreate(resCreated);
+    const resCreated = await Promise.all(
+      body.map(async (value) => {
+        const {
+          imei,
+          vno,
+          vin,
+          lat,
+          lng,
+          lad,
+          led,
+          bp,
+          sts,
+          spd,
+          odo,
+          pow,
+          ss,
+          ns,
+          phn,
+          eml,
+          alerts,
+          aid,
+          id,
+        }: any = value;
+        return this.aerisDataRepository.findOrCreate({
+          where: { id },
+          defaults: {
+            vehicle_number: vno,
+            imei: imei,
+            vin: vin,
+            lat: lat,
+            lng: lng,
+            last_activation_date: lad,
+            license_expire_date: led,
+            battery_level_percentage: bp,
+            status: sts,
+            speed: spd,
+            odometer: odo,
+            power: pow,
+            service_status: ss,
+            phone_number: phn,
+            email: eml,
+            alerts: JSON.stringify(alerts),
+            asset_id: aid,
+            next_service: ns,
+          },
+        });
+      }),
+    );
+    return resCreated;
   }
 }
